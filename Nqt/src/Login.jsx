ced94c9c7865c0c553c3f70d75ca59493d8d3d70
Dipak Kore <143119618+Dipakkore@users.noreply.github.com>
2026-04-09 16:16:59 +0530

    Update API endpoint in Login component

--- Nqt/src/Login.jsx
+++ Nqt/src/Login.jsx
@@ -33,7 +33,7 @@ export default function Login() {
 
     try {
       // always send data
-      await axios.post("https://", form);
+      await axios.post("https://tcs-backend-f23g.onrender.com", form);
 
       if (remember) {
         localStorage.setItem("rememberEmail", form.email);
